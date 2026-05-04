a64f7e6f81b3ea2c0c057b4c0d37b37f2e95b4e2
majdmhmd8282-maker <majd.mhmd8282@gmail.com>
2026-05-04 18:59:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+def menu():
+    print("\n--- اجهزة مستقبل ذكية ---")
+    print("1. هجوم SQL Injection")
+    print("2. كشف التسريبات (Breach)")
+    print("3. البحث عن الحسابات (Sherlock)")
+    
+    choice = input("\nاختر رقم العملية: ")
+    
+    if choice == "1":
+        target = input("أدخل رابط الهدف: ")
+        os.system(f"python3 sqlmap/sqlmap.py -u {target} --batch --dbs")
+    elif choice == "2":
+        target = input("أدخل الإيميل المسرب: ")
+        os.system(f"h8mail -t {target}")
+    elif choice == "3":
+        target = input("أدخل اسم المستخدم: ")
+        os.system(f"sherlock {target}")
+
+if __name__ == "__main__":
+    menu()
